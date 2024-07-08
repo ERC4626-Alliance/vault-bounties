61a2c2b8831f4d00237a89c603baec3465ba1b56
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2024-07-08 06:36:34 -0300

    Initial hardhat (+steroids) boilerplate

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-dependency-compiler");
+require("hardhat-contract-sizer");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+  dependencyCompiler: {
+    paths: ["@ensuro/core/contracts/mocks/TestCurrency.sol", "@ensuro/swaplibrary/contracts/mocks/SwapRouterMock.sol"],
+  },
+};
---
+++ test/test-dummy.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("Dummy tests", function () {
+  it("Knows how to add", async () => {
+    expect(1 + 1).to.equal(2);
+  });
+});
