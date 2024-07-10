68c79b84045da24aafc745344d34fae51de97ba5
PraneshASP <praneshas2000@gmail.com>
2024-07-11 03:26:27 +0530

    chore: factory bug fix

--- velodrome-7575-wrapper/src/VelodromeERC7575VaultFactory.sol
+++ velodrome-7575-wrapper/src/VelodromeERC7575VaultFactory.sol
@@ -6,7 +6,7 @@ import {IPool} from "./interfaces/IPool.sol";
 import {VelodromeERC7575Share} from "./VelodromeERC7575Share.sol";
 import {VelodromeERC7575Vault} from "./VelodromeERC7575Vault.sol";
 
-contract VelodromeERC7575Factory {
+contract VelodromeERC7575VaultFactory {
     event PoolCreated(address indexed shareToken, address indexed vault0, address indexed vault1, address pool);
 
     function createVault(address token0, address token1, IPool pool)
@@ -27,7 +27,7 @@ contract VelodromeERC7575Factory {
         // Create vault for token1
         // TODO: Dynaminally construct vault name and symbol using LibString
         vault1 =
-            address(new VelodromeERC7575Vault(token0, "Vault1", "Symbol1", pool, pool.stable(), ERC20((shareToken))));
+            address(new VelodromeERC7575Vault(token1, "Vault1", "Symbol1", pool, pool.stable(), ERC20((shareToken))));
 
         // Update vault address mapping in share token contract
         VelodromeERC7575Share(shareToken).updateVault(token0, vault0);
